c7aed9b79e03f0c16a1a28603f835f975431f4ab
Megatron <vladyk.baidyk@gmail.com>
2026-03-30 11:59:57 +0300

    new fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/eCommerence/' : '/',
+  base: '/eCommerence/',
   plugins: [react(),tailwindcss()],
 })
